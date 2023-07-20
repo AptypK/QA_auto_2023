eaa3bcd022c5936c6790c5bc487edfcb0b7f7299
Artur Kychenko <artur.kychenko86@gmail.com>
2023-07-20 12:59:40 +0200

    Add new http test

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -5,10 +5,21 @@ import requests
 @pytest.mark.http
 def test_first_request():
     r = requests.get("https://api.github.com/zen")
-    print(r.text)
+    print(f"Response is {r.text}")
 
 
 @pytest.mark.http
 def test_second_request():
     r = requests.get("https://api.github.com/users/defunkt")
-    print(f"Response is{r.text}")
+    # print(f"Response Body is{r.json()}")
+    body = r.json()
+    headers = r.headers
+    assert body["name"] == "Chris Wanstrath"  # Check name
+    assert r.status_code == 200
+    assert headers["Server"] == "GitHub.com"  # Check header {Server}
+
+
+@pytest.mark.http
+def test_status_code_request():
+    r = requests.get("https://api.github.com/users/sergii_butenko")
+    assert r.status_code == 404
